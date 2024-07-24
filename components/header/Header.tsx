a7cd75c9be69413fe1fc12cb16dfcdaec3253601
hanetrchen <474580875@qq.com>
2024-07-24 23:34:08 +0800

    修改页面

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -59,7 +59,7 @@ const Header = () => {
               height={32}
             />
             <span className="text-gray-950 dark:text-gray-300 hidden md:block">
-              AI Anime Art Generator
+              Baby Photoshoot
             </span>
           </Link>
         </div>
--- config/footer.ts
+++ config/footer.ts
@@ -5,52 +5,52 @@ export const footerData: FooterProps = {
   productDescription:
     "A powerful AI-driven platform for creating high-quality anime characters and scenes. Perfect for beginners to easily create stunning anime art without any prior experience.",
   columns: [
-    {
-      title: "Our Products",
-      links: [
-        {
-          name: "AI Tag Generator",
-          title: "AI Tag Generator",
-          href: "https://ai-tag-generator.com",
-        },
-      ],
-    },
-    {
-      title: "Useful Links",
-      links: [
-        {
-          name: "Woy AI Tools",
-          title: "Woy AI Tools Directory",
-          href: "https://woy.ai",
-        },
-        {
-          name: "Awesome AI Directory",
-          title: "Awesome AI Directory Tools Directory",
-          href: "https://awesomeai.cc",
-        },
-        {
-          name: "Gen AI Prism Tools",
-          title: "Gen AI Prism Tools Directory",
-          href: "https://genaiprism.site/",
-        },
-      ],
-    },
-    {
-      title: "Resources",
-      links: [
-        {
-          name: "Docs",
-          title: "AI Anime Art Generator Documentation",
-          href: "/",
-        },
-        { name: "Blog", title: "AI Anime Art Generator Blog", href: "/" },
-        {
-          name: "Contact Us",
-          title: "Contact Us",
-          href: "mailto:enterwizdev@outlook.com",
-        },
-      ],
-    },
+    // {
+    //   title: "Our Products",
+    //   links: [
+    //     {
+    //       name: "AI Tag Generator",
+    //       title: "AI Tag Generator",
+    //       href: "https://ai-tag-generator.com",
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: "Useful Links",
+    //   links: [
+    //     {
+    //       name: "Woy AI Tools",
+    //       title: "Woy AI Tools Directory",
+    //       href: "https://woy.ai",
+    //     },
+    //     {
+    //       name: "Awesome AI Directory",
+    //       title: "Awesome AI Directory Tools Directory",
+    //       href: "https://awesomeai.cc",
+    //     },
+    //     {
+    //       name: "Gen AI Prism Tools",
+    //       title: "Gen AI Prism Tools Directory",
+    //       href: "https://genaiprism.site/",
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: "Resources",
+    //   links: [
+    //     {
+    //       name: "Docs",
+    //       title: "AI Anime Art Generator Documentation",
+    //       href: "/",
+    //     },
+    //     { name: "Blog", title: "AI Anime Art Generator Blog", href: "/" },
+    //     {
+    //       name: "Contact Us",
+    //       title: "Contact Us",
+    //       href: "mailto:enterwizdev@outlook.com",
+    //     },
+    //   ],
+    // },
   ],
 };
 
