32bf00c60ed6f6db981cc831780e11ccb047dc5f
hanterchen <202301Tas**>
2024-08-21 19:46:18 +0800

    上传到oss

--- actions/faceMeger.ts
+++ actions/faceMeger.ts
@@ -14,23 +14,27 @@ export async function faceswapAI(swapImage: string, templateImageUrl: string) {
   // 延迟3秒查询
   await delay(7000);
   // //3.请求查询requestId返回base64结果   
-  let resultData= null;
+  let finalResultData= null;
   while (true) {
-    await delay(2000);
+    await delay(3000);
     let resultData = await getResult(requestId);
-    if (resultData != 'InProgress') {
+    if(resultData==null){
+      continue
+    }
+    if (resultData != 'InProgress') {  
+      finalResultData = resultData;
       break;
     }
   }
   //计算耗时
   console.log('耗时：' + (Date.now() - timestamp) / 1000 + '秒');  
-  console.log('resultData:', resultData);  
+  console.log('resultData:', finalResultData);  
   // const resultUrl ='http://worker-images.ws.pho.to/i2/4531c195176f4f1edd6618d6dd4783c1ef5cef1a_result.jpeg';
   //4.上传到oss,todo 失败要重试
-  if (resultData == null) {
+  if (finalResultData == null) {
     throw new Error('faceswapAI failed result is empty');
   }
-  const ossUrl = await downloadAndUploadImage(resultData)
+  const ossUrl = await downloadAndUploadImage(finalResultData)
   //5.todo 上传oss的文件？后续处理   
 
   //6.返回结果
